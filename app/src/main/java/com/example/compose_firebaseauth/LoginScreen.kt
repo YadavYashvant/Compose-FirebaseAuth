2e9fdb56d791153d2542bd894bd9ad32d41e20c3
Yashvant Yadav <yashvantyadav855@gmail.com>
2023-09-15 16:19:03 +0530

    making sign in page

---
+++ app/src/main/java/com/example/compose_firebaseauth/LoginScreen.kt
@@ -0,0 +1,17 @@
+package com.example.compose_firebaseauth
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen() {
+    ElevatedCard(
+        modifier = Modifier
+            .padding(16.dp)
+    ) {
+
+    }
+}
--- app/src/main/java/com/example/compose_firebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/compose_firebaseauth/MainActivity.kt
@@ -3,13 +3,36 @@ package com.example.compose_firebaseauth
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose_firebaseauth.ui.theme.ComposeFirebaseAuthTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +45,91 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .padding(horizontal = 10.dp)
+                    ) {
+
+                        OutlinedCard  (
+                            modifier = Modifier
+                                .padding(horizontal = 12.dp, vertical = 25.dp)
+                                .fillMaxWidth()
+                                .wrapContentHeight(),
+                            shape = MaterialTheme.shapes.large,
+                        ) {
+                            Column {
+                                Text(
+                                    text = "Register or log in",
+                                    fontSize = 30.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier
+                                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                                )
+                                Textfield()
+                                TextfieldEmail()
+
+                                Row(
+                                    modifier = Modifier
+                                        .padding(vertical = 16.dp, horizontal = 10.dp)
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    OutlinedButton(onClick = { /*TODO*/ }) {
+                                        Text(text = "Sign in", Modifier.padding(10.dp))
+                                    }
+
+                                    OutlinedButton(onClick = { /*TODO*/ }) {
+                                        Text(text = "Register", Modifier.padding(10.dp))
+                                    }
+                                }
+                            }
+                        }
+
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Textfield() {
+    val inputvalue = remember { mutableStateOf(TextFieldValue()) }
+    OutlinedTextField(value = inputvalue.value,
+        onValueChange = {inputvalue.value = it},
+        placeholder = { Text(text = "Enter user name ")},
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp, horizontal = 10.dp),
+
+        shape = MaterialTheme.shapes.large,
+        colors = TextFieldDefaults.textFieldColors(
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent
+        )
     )
+
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    ComposeFirebaseAuthTheme {
-        Greeting("Android")
-    }
+fun TextfieldEmail() {
+
+    val inputvalue = remember { mutableStateOf(TextFieldValue()) }
+    OutlinedTextField(value = inputvalue.value,
+        onValueChange = {inputvalue.value = it},
+        placeholder = { Text(text = "Enter email id ")},
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp, horizontal = 10.dp),
+
+        shape = MaterialTheme.shapes.large,
+        colors = TextFieldDefaults.textFieldColors(
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent
+        )
+    )
+
 }
